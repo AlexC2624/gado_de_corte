8a864812ed2d5f37855ea4fc9f50a531a9540ca8
AlexC2624 <alex.cargnin.2006@gmail.com>
2025-04-07 13:25:37 -0300

    Criação das funções escolher_usuario e salvar_acesso, também teve edição em buscar_users

--- loguin.py
+++ loguin.py
@@ -1,20 +1,41 @@
+import os
 from armazenamento import Banco
 
 class Loguin:
     def __init__(self):
-        self.banco = Banco('usuarios.csv', cabecalho= ['nome', 'senha', 'tipo'])
+        self.banco = Banco('usuario.csv', cabecalho= ['nome', 'senha', 'tipo'])
     
     def buscar_users(self):
-        users = self.banco.ler()
-        print(users)
-        usuarios = {}
-        for linha in users[1:]:
-            usuarios[linha[0]] = {users[0][1]: linha[1], users[0][2]: linha[0][2]}
+        admin = 'admin'
+        user = 'user'
+        usuarios = {
+            admin: {'senha': admin, 'tipo': admin},
+            user: {'senha': user, 'tipo': user}
+        }
         return usuarios
+    
+    def escolher_usuario(self, usuarios: dict):
+        os.system('clear')
+        print('\tFaça o seu loguin')
+        w = 0
+        while w < 3:
+            w += 1
+            usuario = input('Nome: ').strip()
+            if usuario in list(usuarios.keys()):
+                senha = input('Senha: ').strip()
+                if senha == usuarios[usuario]['senha']:
+                    return {'usuario': usuario, 'tipo': usuarios[usuario]['tipo']}
+                else:
+                    w = 0
+                    print('Senha incorreta!')
+            else: print('Usuário não cadastrado!!')
+    
+    def salvar_acesso(self):
+        self.banco.escrever()
 
     def start(self):
-        users = self.buscar_users()
-        print(users)
+        usuario = self.escolher_usuario(self.buscar_users())
+        print(usuario)
 
 if __name__ == '__main__':
     L = Loguin()
