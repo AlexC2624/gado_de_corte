4a406a1956e4c66ec5b2a6ac96a58922c293ed05
AlexC2624 <alex.cargnin.2006@gmail.com>
2025-04-08 18:41:06 -0300

    Inicialização do app do Flask e a estrutura base do user_1

--- servidor.py
+++ servidor.py
@@ -1,2 +1,23 @@
-from flask import Flask
+from main import start
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+SENHA_CORRETA = '12345678'
+
+@app.route('/', methods=['GET'])
+def index(): return False, 'Esta página não pode ser acessada por navegador!!!'
+
+@app.route('/user-1', methods=['POST'])
+def user_1():
+    if request.is_json:
+        dados = request.get_json()
+        senha = dados.get('senha')
+        if senha == SENHA_CORRETA:
+            pass
+        else: return False, 'A senha informada esta incorreta!!!'
+    else: return False, 'O nome de usuário está incoreto!!!'
+
+def iniciar(): app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == '__main__': app.run(debug=True, host='0.0.0.0', port=5000)
