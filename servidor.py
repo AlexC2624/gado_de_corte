1fd6898a632eaebb6485fe073178f9a459220f51
AlexC2624 <alex.cargnin.2006@gmail.com>
2025-04-09 12:48:35 -0300

    Editado a rota / para recusar GET e criado a rota /usuario que por meio de POST recebe o mone do usuario e a senha

--- servidor.py
+++ servidor.py
@@ -3,20 +3,30 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-SENHA_CORRETA = '12345678'
+usuarios = {
+    'user1': {'senha': '123'}
+}
 
 @app.route('/', methods=['GET'])
-def index(): return False, 'Esta página não pode ser acessada por navegador!!!'
+def index(): return 'Esta página não pode ser acessada por navegador, entre em contato com o desenvolvedor para mais informações!'
 
-@app.route('/user-1', methods=['POST'])
-def user_1():
+@app.route('/usuario', methods=['POST'])
+def usuario():
     if request.is_json:
         dados = request.get_json()
+        user_nane = dados.get('user_name')
         senha = dados.get('senha')
-        if senha == SENHA_CORRETA:
-            pass
-        else: return False, 'A senha informada esta incorreta!!!'
-    else: return False, 'O nome de usuário não está cadastrado!!!'
+        print(dados, user_nane, senha, sep='\n')
+
+        if user_nane in list(usuarios.keys()):
+            if senha == usuarios[user_nane]['senha']:
+                print('acessado com sucesso')
+                return jsonify({'mensagem': 'Acesso concedido com sucesso!'}), 200
+            else:
+                return jsonify({'erro': 'A senha informada está incorreta!'}), 401
+        else: return jsonify({'erro': 'O usuario não está cadastrado!'}), 404
+    else:
+        return jsonify({'erro': 'Requisição precisa estar no formato JSON!'}), 400
 
 def iniciar(): app.run(debug=True, host='0.0.0.0', port=5000)
 
