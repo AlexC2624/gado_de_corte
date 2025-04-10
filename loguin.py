2fc6ceb2c5a56bfe360b657f055c83e3ef0f825a
AlexC2624 <alex.cargnin.2006@gmail.com>
2025-04-10 19:38:02 -0300

    Configuração inicial na def buscar_acesso

--- loguin.py
+++ loguin.py
@@ -1,12 +1,19 @@
-import os
+from servidor import start
 from armazenamento import Banco
 
 class Loguin:
     def __init__(self):
-        self.banco = Banco('usuario.csv', cabecalho= ['Nome', ])
-    
+        self.banco = Banco('conexao.csv', cabecalho= ['tipo', 'endereco', 'porta']) # tipo= servidor/cliente, 
+    pass
     def buscar_acesso(self):
-        pass
+        """Procura pelos dados do acesso anterior.
+
+        Returns:
+            bool: Se não encontrar registros retorna False.
+            list[list]: O conteúdo salvo do acesso anterior que pode ser usado para conctar novamente automaticamente.
+        """
+        if self.banco.contagem() == 0: return False
+        return self.banco.ler()[1:]
     
     def buscar_users(self):
         pass
