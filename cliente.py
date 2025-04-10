c20ca49f50980d163ff4a545e524c7d19499081c
AlexC2624 <alex.cargnin.2006@gmail.com>
2025-04-10 18:13:16 -0300

    Troca do Flask para a conexão con socket

--- cliente.py
+++ cliente.py
@@ -1,12 +1,22 @@
-import requests
+import socket
 
-url = 'http://127.0.0.1:5000/usuario'
-dados = {
-    'user_name': 'user1',
-    'senha': '123'  # experimente mudar para testar
-}
+HOST = '127.0.0.1'  # IP do servidor
+PORT = 5000
 
-resposta = requests.post(url, json=dados)
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect((HOST, PORT))
 
-print('Status:', resposta.status_code)
-print('Resposta:', resposta.json())
+loop = True
+while loop:
+    enviar = input('Digite a mensagem: ').strip()
+    if enviar == '1': break
+    if enviar == '': continue
+
+    cliente.sendall(enviar.encode())
+
+    resposta = cliente.recv(1024)
+    resposta = resposta.decode()
+    print(resposta)
+
+cliente.close()
+print('Conexão finalizada')
