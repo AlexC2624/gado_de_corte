a8fd8e7ecfcaccc0b6d640297efc92fc37e056b8
AlexC2624 <alex.cargnin.2006@gmail.com>
2025-04-12 23:05:25 -0300

    Criação da def formulário

--- loguin.py
+++ loguin.py
@@ -1,10 +1,46 @@
+import os
 from servidor import start
 from armazenamento import Banco
 
 class Loguin:
     def __init__(self):
-        self.banco = Banco('conexao.csv', cabecalho= ['tipo', 'endereco', 'porta']) # tipo= servidor/cliente, 
-    pass
+        self.banco = Banco('conexao.csv', cabecalho= ['tipo', 'endereco', 'porta']) # tipo= servidor/cliente
+    
+    def __limpar_tela__(self):
+        """
+        Limpa a tela do terminal para melhorar a visualização do menu.
+
+        O comando utilizado para limpar a tela é:
+            - 'cls' para sistemas Windows.
+            - 'clear' para sistemas Unix-based (Linux, macOS).
+        """
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def formulario(self):
+        self.__limpar_tela__()
+        print('\tConfiguração inicial')
+        print('1 - Servidor')
+        print('2 - Cliente')
+        print('3 - Sair')
+        execusoes = 0
+        while execusoes < 4:
+            execusoes += 1
+            try:
+                escolha = int(input('>>> ').strip())
+                if escolha == 1:
+                    ip_cliente = input('\n Digite o ip do cliente ').strip()
+                    porta = int(input(' Digite a porta de coneção ').strip())
+                    self.salvar_acesso(['servidor', ip_cliente, porta])
+                    break
+                else: raise ValueError
+            except ValueError:
+                print('Digite um número válido!')
+                continue
+    
+    def salvar_acesso(self, dados):
+        self.banco.escrever(dados, sobrescrever=True)
+        return True
+
     def buscar_acesso(self):
         """Procura pelos dados do acesso anterior.
 
@@ -14,18 +50,9 @@ class Loguin:
         """
         if self.banco.contagem() == 0: return False
         return self.banco.ler()[1:]
-    
-    def buscar_users(self):
-        pass
-    
-    def escolher_usuario(self):
-        pass
-    
-    def salvar_acesso(self):
-        pass
 
     def start(self):
-        pass
+        self.formulario()
 
 if __name__ == '__main__':
     L = Loguin()
